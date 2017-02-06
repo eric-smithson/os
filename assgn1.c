47b2099f1492b1e210319694a38fade9b8a75c2b
neal <nealnguyen@knights.ucf.edu>
2017-02-06 11:10:27 -0500

    parsing finished

--- assgn1.c
+++ assgn1.c
@@ -1,46 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct process
+typedef struct process
 {
 	char* name;
 	int arrival;
 	int burst;
 
-}process;
+}Process;
 int main(void)
 {
+	Process* processes;
 	char buffer[255];
 	FILE *fp;
 	char* token;
+	int numberOfProcesses;
+	char* typeOfScheduler;
+	int timeUnit;
 	fp = fopen("./set1_process.in","r");
+	int lineNumber = 0;
+	int timeQuantum;
+	char** inputStrings;
 	int i = 0;
-	while(fgets(buffer,sizeof buffer, fp) != NULL)
+	if(fgets(buffer,sizeof buffer, fp)!= NULL)
 	{
-		char* newString;
-		newString = (char*) malloc(strlen(buffer));
-
-		strtok(buffer,"#");
-
-		strcpy(newString,buffer);
-
-
-
-		if(strcmp(buffer,"end\n") == 0)
-		{
-			printf("finished\n");
-			return 0;
-		}
-		if(i >3)
-		{
-			token = strtok(newString," ");
-			while(token != NULL)
-			{
-				printf("%s \n",token);
-				token = strtok(NULL," ");
-			}
-		}
-		free(newString);
-		i++;
+		token = strtok(buffer," ");
+		numberOfProcesses = atoi(strtok(NULL," "));
 	}
+	processes = malloc(sizeof(Process) * numberOfProcesses);
+	if(fgets(buffer,sizeof buffer,fp) !=NULL)
+	{
+		token = strtok(buffer," ");
+		timeUnit = atoi(strtok(NULL, " "));
+	}
+	printf("time unit = %d\n",timeUnit);
+	if(fgets(buffer,sizeof buffer,fp) != NULL)
+	{
+		token = strtok(buffer," ");
+
+		typeOfScheduler = (strtok(NULL," "));
+
+	}
+	printf("type of scheduler: %s\n",typeOfScheduler);
+	if(fgets(buffer,sizeof buffer,fp) != NULL)
+	{
+		token = strtok(buffer," ");
+		timeQuantum = atoi(strtok(NULL," "));
+	}
+	printf("time quantum = %d\n", timeQuantum);
+	for(i = 0; i < numberOfProcesses;i++)
+	{
+		if(fgets(buffer,sizeof buffer,fp) != NULL)
+		{
+			token = strtok(buffer," ");
+			strtok(NULL," ");
+			processes[i].name = strtok(NULL," ");
+			strtok(NULL," ");
+			processes[i].arrival = atoi(strtok(NULL," "));
+			strtok(NULL," ");
+			processes[i].burst = atoi(strtok(NULL," "));
+
+		}
+		printf("process name: %s arrival: %d burst: %d\n", processes[i].name,processes[i].arrival,processes[i].burst);
+	}
+
 }
