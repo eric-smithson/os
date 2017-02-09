b9ab4d3b3a0b2199813c972f59a94ab0a7cc58e6
JessicaLaurenB <mathlove1255@gmail.com>
2017-02-09 15:58:31 -0500

    done with rr

--- rr.c
+++ rr.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "assgn1.h"
@@ -35,24 +35,25 @@ void enqueueRoundRobin(Process* process)
 		front = rear = current = temp;
 		return;
 	}
-		rear->nextNode = temp;
-		rear = temp;
+
+	rear->nextNode = temp;
+	rear = temp;
 }
 
 
-void dequeueP(Process* process_done)
+void dequeueRoundRobin(Process* process_done)
 {
 	int i;
-
 	temp = front;
+
 	if(temp == NULL)
 	{
 		//printf("Queue is empty\n");
 		return;
 	}
-
 	
-	if(temp->nextNode == NULL) // for when the last process is dequeued
+	// when the last process is dequeued
+	if(temp->nextNode == NULL)
 	{
 		free(front->name);
 		free(front);
@@ -80,47 +81,34 @@ void dequeueP(Process* process_done)
 
 void runRoundRobin(Process* processArray,int sizeOfProcessArray,int totalRunTime, int quantum)
 {
-	// Variables
 	int time = 0; // counter acts like a clock, counting up until totalRunTime
-	int numOfProcessesRemaining = sizeOfProcessArray;
 	front = rear = NULL;
-
-	int i;
-	int arrival_time[sizeOfProcessArray];
-	int finished_time[sizeOfProcessArray];
+	int i, arrival_time[sizeOfProcessArray], finished_time[sizeOfProcessArray]; ;
+	int quantum_counter=0, turnaround_time, wait_time;
 
 	// to calculate turnaround time, record the time when a process arrives and finishes
 	// turnaround_time = finished_time - arrival_time
 	// to calculate wait time, 
 	// wait_time = turnaround_time - burst_size
 
-	for(i = 0; i < sizeOfProcessArray;i++)
-	{
-		arrival_time[i] = -1;
-		finished_time[i] = -1;
-	}
-
 	// top section of output
 	printf("\n%d processes\n",sizeOfProcessArray);
 	printf("Using Round-Robin\n");
 	printf("Quantum %d\n\n",quantum);
 
+	// Quicksort by arrival time
+	qsort(processArray, sizeOfProcessArray, sizeof(Process), compare_arrival);
 
-	qsort(processArray, sizeOfProcessArray, sizeof(Process), compare_arrival); // quicksort average O(nlogn)
-
-	for(i=0; i<sizeOfProcessArray; ++i) // loop for testing purposes
+	// initializing all to -1, processes that did not arrive or finish will not be calculated
+	for(i = 0; i < sizeOfProcessArray;i++)
 	{
-		//printf("process %s arrival time: %d\n",processArray[i].name, processArray[i].arrival);
+		arrival_time[i] = -1;
+		finished_time[i] = -1;
+		// index in process struct will coorespond to arrival time/finish time arrays
 		processArray[i].index = i;
 	}
 
-	int currentP = 0; // counter that keeps track of current process in the CPU
-	int nextP = 0; // counter that keeps tack of the next process to arrive
-
-	int quantum_counter=0;
-
-
-
+	// task scheduling for loop 
 	for(time = 0; time < totalRunTime ; time++)
 	{
 
@@ -137,65 +125,61 @@ void runRoundRobin(Process* processArray,int sizeOfProcessArray,int totalRunTime
 
 		if(current != NULL)
 		{
-
-		if(current->burst == 0)
-		{
-			
-			if(!isEmpty())
+			if(current->burst == 0)
 			{
-				printf("Time %d: %s finished\n",time,current->name);
-				finished_time[current->index] = time;
-				//printf("current index i %d\n",current->index);
-			}	
-			if(current == front)
-				dequeue();
-			else
-				dequeueP(current);
-			quantum_counter = quantum;
 			
+				if(!isEmpty())
+				{
+					printf("Time %d: %s finished\n",time,current->name);
+					finished_time[current->index] = time;
+					//printf("current index i %d\n",current->index);
+				}
+
+				if(current == front)
+					dequeue();
+				else
+					dequeueRoundRobin(current);
+
+				quantum_counter = quantum; // move to next process
+			}
+
+			if(quantum_counter == quantum)
+			{
+				// switch to next process, reset quantum_counter
+				quantum_counter = 0;
+
+				if(isEmpty() == 0 && current->nextNode != NULL)
+					current = current->nextNode;
+				else if(isEmpty() == 0 && current->nextNode == NULL)
+					current = front;
+			}
+
+			if(current->burst > 0)
+			{
+				quantum_counter++;
+				printf("Time %d: %s selected (burst %d)\n",time,current->name,current->burst);
+				current->burst--;
+			}	
+
+			// if the queue is empty
+			if(isEmpty() == 1)
+				printf("Time %d: IDLE\n",time);	
+
 		}
 
-	
-		if(quantum_counter == quantum)
-		{
-			// switch to next process
-			quantum_counter = 0;
+	} // end of task scheduling for loop
 
-			if(isEmpty() == 0 && current->nextNode != NULL)
-				current = current->nextNode;
-			else if(isEmpty() == 0 && current->nextNode == NULL)
-				current = front;
-
-		}
-
-		if(current->burst > 0)
-		{
-			quantum_counter++;
-			printf("Time %d: %s selected (burst %d)\n",time,current->name,current->burst);
-			current->burst--;
-		}	
-
-		if(isEmpty() == 1)
-			printf("Time %d: IDLE\n",time);	
-
-		}
-
-	}
-
+	// Display finished time or which processes didn't finish 
 	if(isEmpty() == 1)
-		printf("Finished at time %d\n",totalRunTime);
+		printf("Finished at time %d\n\n",totalRunTime);
 	else
 		while(!isEmpty())
 		{
-			printf("Process %s did not finish\n",front->name);
+			printf("Process %s did not finish\n\n",front->name);
 			dequeue();
-		}
+		}	
 
-	printf("\n");	
-
-	int turnaround_time;
-	int wait_time;
-
+	// printing the wait and turnaround times for all processes that finished
 	for(i = 0; i < sizeOfProcessArray;i++)
 	{
 		//printf("Arrival time of %s: %d  Finish time: %d\n",processArray[i].name,arrival_time[i],finished_time[i]);
@@ -209,7 +193,6 @@ void runRoundRobin(Process* processArray,int sizeOfProcessArray,int totalRunTime
 	 		,wait_time
 	 		,turnaround_time);
 		}
-
 	}
 
 }
